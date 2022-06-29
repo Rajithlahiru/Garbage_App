5e95172c55c3605ea7ef69c5af7691ee9c280fbe
Rajith Lahiru <rajithlahiru7@gmail.com>
2022-06-30 00:19:06 +0530

    sign in and sign up added

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name="dashboard"),
+    path('signup',views.signup,name="signup"),
+    path('signin',views.signin,name="signin"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'dashboard/index.html')
+
+def signup(request):
+    return render(request, 'dashboard/signup.html')
+
+def signin(request):
+    return render(request, 'dashboard/signin.html')
