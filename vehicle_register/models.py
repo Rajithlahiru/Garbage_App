58890789a62f98fe803236af18507e2198d954b4
Rajith Lahiru <rajithlahiru7@gmail.com>
2022-08-27 22:56:59 +0530

    add default value

--- vehicle_register/models.py
+++ vehicle_register/models.py
@@ -7,7 +7,7 @@ class Vehicle(models.Model):
     vehicle_type = models.CharField(max_length=50)
     vehicle_model = models.CharField(max_length=100)
     vehicle_no = models.CharField(max_length=50)
-    owner_name= models.CharField(max_length=100)
+    owner_name= models.CharField(max_length=100,default="driver")
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
 
