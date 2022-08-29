35c08a5dedc3e7160d21b4ec3754218a958af81e
rushanJ <rushanthasindu10@gmail.com>
2022-08-29 23:26:47 +0530

    fix

--- garbageapp/settings.py
+++ garbageapp/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = "."
+BASE_DIR = "./"
 
 
 # Quick-start development settings - unsuitable for production
