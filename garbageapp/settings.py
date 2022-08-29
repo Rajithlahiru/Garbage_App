7ac8ecd45cf726f9b3b6ea379bf3af9f9b05a4b6
rushanJ <rushanthasindu10@gmail.com>
2022-08-29 23:44:22 +0530

    fix

--- garbageapp/settings.py
+++ garbageapp/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-PROJECT_APP_PATH = os.path.dirname(os.path.abspath(__file__))
+PROJECT_APP_PATH = '/home/ubuntu/Garbage_App'
 
 # BASE_DIR = "./"
 print (BASE_DIR)
