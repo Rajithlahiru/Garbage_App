273edd762faca41cbabcc451222e5798ed3944d3
rushanJ <rushanthasindu10@gmail.com>
2022-08-30 00:09:26 +0530

    fix

--- garbageapp/settings.py
+++ garbageapp/settings.py
@@ -15,10 +15,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-PROJECT_APP_PATH = '/home/ubuntu/Garbage_App'
+print(BASE_DIR)
 
-# BASE_DIR = "./"
-print (os.path.join(PROJECT_APP_PATH, '/templates/'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -71,7 +69,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['Garbage_App/templates/'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
