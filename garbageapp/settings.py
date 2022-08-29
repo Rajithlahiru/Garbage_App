954cf10253c9a1b6b32a10fb5df4266fcb4db390
rushanJ <rushanthasindu10@gmail.com>
2022-08-29 23:28:36 +0530

    fix

--- garbageapp/settings.py
+++ garbageapp/settings.py
@@ -16,8 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # BASE_DIR = "./"
-
-
+print (BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
