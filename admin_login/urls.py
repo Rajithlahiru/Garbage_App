c921570d26814e0c5d9772fb15952796bc3b5bfa
Rajith Lahiru <rajithlahiru7@gmail.com>
2022-11-11 14:33:53 +0530

    landing page

--- admin_login/urls.py
+++ admin_login/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("signup", views.signup, name='signup'),
-    path("signin", views.signin, name='signin'),
+    # path("signup", views.signup, name='signup'),
+    path("", views.signin, name='signin'),
 ]
--- admin_login/views.py
+++ admin_login/views.py
@@ -9,30 +9,30 @@ from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
-def signup(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        password2 = request.POST.get('password2')
+# def signup(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         email = request.POST.get('email')
+#         password = request.POST.get('password')
+#         password2 = request.POST.get('password2')
 
-        if password == password2:
-            if User.objects.filter(email = email).exists():
-                messages.info(request, 'email taken')
-                return redirect('signup')
-            elif User.objects.filter(username = username).exists():
-                messages.info(request, 'username already taken')
-                return redirect('signup')
-            else:
-                user = User.objects.create_user(username=username, email=email, password=password)
-                user.save()
-                return redirect('signin')
-        else:
-            messages.info(request, 'password not matching')
-            return redirect('signup') 
+#         if password == password2:
+#             if User.objects.filter(email = email).exists():
+#                 messages.info(request, 'email taken')
+#                 return redirect('signup')
+#             elif User.objects.filter(username = username).exists():
+#                 messages.info(request, 'username already taken')
+#                 return redirect('signup')
+#             else:
+#                 user = User.objects.create_user(username=username, email=email, password=password)
+#                 user.save()
+#                 return redirect('signin')
+#         else:
+#             messages.info(request, 'password not matching')
+#             return redirect('signup') 
 
-    else:
-        return render(request,"admin_login/signup.html")
+#     else:
+#         return render(request,"admin_login/signup.html")
 
 
 # def signin(request):
